c5dfae1411ce6747d8a326f19b5f96a141e86889
joneirikboska <joneirikboska@gmail.com>
2021-01-18 18:51:57 +0100

    Install gatsby-image and create correct query and options for rich text.

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,25 +1,47 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/Layout"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
+import { BLOCKS } from "@contentful/rich-text-types"
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            title
+            fluid(maxWidth: 750) {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
       }
-      html
     }
   }
 `
 
-const Blog = ({ data: { markdownRemark: post } }) => {
+const Blog = ({ data: { contentfulBlogPost: post } }) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const { title } = node.data.target
+
+        return <Img {...node.data.target} alt={title} />
+      },
+    },
+  }
   return (
     <Layout>
-      <h1>{post.frontmatter.title}</h1>
-      <p>{post.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      <h1>{post.title}</h1>
+      <p>{post.publishedDate}</p>
+      {renderRichText(post.body, options)}
     </Layout>
   )
 }
