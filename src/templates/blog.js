2685cc6c296af6868275bb9edc171f2c589ed628
joneirikboska <joneirikboska@gmail.com>
2021-01-18 11:51:40 +0100

    Create template file and add post content from query.

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,8 +1,27 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 
-const Blog = () => {
-  return <Layout>This is the blog template</Layout>
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = ({ data: { markdownRemark: post } }) => {
+  return (
+    <Layout>
+      <h1>{post.frontmatter.title}</h1>
+      <p>{post.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+    </Layout>
+  )
 }
 
 export default Blog
